e1c06dc6f80e49c8de16c4a4fac52d96b9206524
songjunhwa <ghd581@gmail.com>
2024-07-01 09:20:49 +0900

    반복문 연습 07/01

---
+++ src/loop/While2_2.java
@@ -0,0 +1,20 @@
+package loop;
+
+public class While2_2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1;
+        int endNum = 3;
+
+        sum = sum + i;
+        System.out.println("i=" + i + " sum="+sum);
+        i++;
+
+        sum = sum + i;
+        System.out.println("i=" + i + " sum="+sum);
+        i++;
+
+        sum = sum + i;
+        System.out.println("i=" + i + " sum="+sum);
+    }
+}
---
+++ src/loop/ex/WhileEx3.java
@@ -0,0 +1,14 @@
+package loop.ex;
+
+public class WhileEx3 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int max = 100;
+        int i = 1;
+        while (i <= max) {
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
